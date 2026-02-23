7aa640a4fe58b3048f0ca16659027005ea503c91
EUROPE\A0858698 <naiyar.ansari.contractor@arcelormittal.com>
2026-02-23 14:51:51 +0530

    s3 bucket

--- terraform_project/output.tf
+++ terraform_project/output.tf
@@ -1,9 +1,9 @@
-output "ec2_public_ip" {
-  description = "Public IP of EC2"
-  value       = aws_instance.public_ec2.public_ip
-}
+##output "ec2_public_ip" {
+ ## description = "Public IP of EC2"
+  ##value       = aws_instance.public_ec2.public_ip
+#}
 
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.my_vpc.id
-}
+#output "vpc_id" {
+ # description = "VPC ID"
+  #value       = aws_vpc.my_vpc.id 
+#}
